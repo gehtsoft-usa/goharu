2a6bf03f8aba59d11336bde9537d6de834f26cde
Nikolay Gekht <F:\data\thebat>
2019-08-09 11:06:17 -0400

    statistics is not needed anymore, porting is finished

--- libharu/stat.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo to be done:
-findstr /r /n "^\-" libharu.txt | find /c ":"
-echo done:
-findstr /r /n "^\+" libharu.txt | find /c ":"
-echo question:
-findstr /r /n "^\?" libharu.txt | find /c ":"
